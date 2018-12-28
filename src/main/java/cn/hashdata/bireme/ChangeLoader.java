b71b5000bca60845626fc6996b7ae715a2166e8a
liyangyang <321678012@qq.com>
2018-12-28 12:38:00 +0800

    日志

--- src/main/java/cn/hashdata/bireme/ChangeLoader.java
+++ src/main/java/cn/hashdata/bireme/ChangeLoader.java
@@ -247,7 +247,7 @@ public class ChangeLoader implements Callable<Long> {
             try {
                 Table tableNew= MysqlToPgDdlUtil.reflushTableAfterDDl(fullTableName,conn,cxt);
                 this.table = tableNew;
-                logger.info("------------------更新表结构结束------beforeTable:{}-----afterTable--fullTableName{}",this.table.tableFullName,fullTableName);
+                logger.info("------------------更新表结构结束------beforeTable:{}-----afterTable:{}",this.table.tableFullName,fullTableName);
             } catch (Exception e) {
                 logger.error("---ddl语句执行后，获取更新后的表结构异常：table:{}",fullTableName,e);
             }
